9cf91902540f1e45d5d5bdb8e25579eaafa8e3cb
CraigWhittington <cfwhit@hotmail.com>
2020-05-15 14:34:53 +0100

    Updates

---
+++ precache-manifest.dc70855701d24ef93ff69afe0a4746f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60c7e3a3461ee5851e0ed483ec4d812",
+    "url": "/Dadjokes/index.html"
+  },
+  {
+    "revision": "a0b33dc2b39d5016f05a",
+    "url": "/Dadjokes/static/css/main.0f375535.chunk.css"
+  },
+  {
+    "revision": "b879129e62e6592e6546",
+    "url": "/Dadjokes/static/js/2.78ebaecd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dadjokes/static/js/2.78ebaecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b33dc2b39d5016f05a",
+    "url": "/Dadjokes/static/js/main.93850ddb.chunk.js"
+  },
+  {
+    "revision": "79bfb52750985d57ef09",
+    "url": "/Dadjokes/static/js/runtime-main.1310a8dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dadjokes/static/media/logo.5d5d9eef.svg"
+  }
+]);
